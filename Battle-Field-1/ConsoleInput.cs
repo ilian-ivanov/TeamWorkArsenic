d2876b7f4a6a8209e6458c6c03e861f17304ab2a
Galin Krastev <galinkrastev@gmail.com>
2013-05-14 21:58:00 +0300

    Extracted new class ConsoleInput
    ConsoleInput validates and makes shure that the input is valid for the
    engin to run

---
+++ Battle-Field-1/ConsoleInput.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+
+namespace BattleField
+{
+    class ConsoleInput
+    {
+        public static void ReadPlayerMove(int n, string[,] workField, out int x, out int y)
+        {
+            bool isCorrectUserMove = false;
+            int row;
+            int col;
+
+            do
+            {
+                Console.WriteLine("Please enter coordinates: ");
+                String userInput = Console.ReadLine();
+
+                bool isValidUserInput = TryParseValidUserInput(userInput, out row, out col);
+                bool areCoordinates = IsInputValidCoordinate(n, row, col);
+                bool isValidMove = false;
+
+                if (isValidUserInput && areCoordinates)
+                {
+                    isValidMove = IsPlayerValidMove(workField, row, col);
+                }
+
+                if (isValidUserInput && areCoordinates && isValidMove)
+                {
+                    isCorrectUserMove = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid move! ");
+                }
+            }
+            while (!isCorrectUserMove);
+
+            x = row + GameState.GameFieldScaleOffset;
+            y = col * 2 + GameState.GameFieldScaleOffset;
+        }
+
+        private static bool TryParseValidUserInput(string userInput, out int row, out int col)
+        {
+            bool isValidRow = false;
+            bool isValidCol = false;
+            string rowCol = userInput;
+            try
+            {
+                isValidRow = int.TryParse(rowCol.Substring(0, 1), out row);
+                isValidCol = int.TryParse(rowCol.Substring(2, 1), out col);
+            }
+            catch
+            {
+                // we dont care why the input is not parsed
+                // that's why no specific exception is catched
+                // we only need correct input
+                // row and col must have some int value after the method is called
+                row = -1;
+                col = -1;
+            }
+
+            if (isValidRow && isValidCol)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool IsPlayerValidMove(string[,] workField, int row, int col)
+        {
+            int gameFieldRow = row + GameState.GameFieldScaleOffset;
+            int gameFieldCol = col * 2 + GameState.GameFieldScaleOffset;
+            if (workField[gameFieldRow, gameFieldCol] == "-" ||
+                workField[gameFieldRow, gameFieldCol] == "X")
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static bool IsInputValidCoordinate(int n, int row, int col)
+        {
+            if ((row < 0 || row > (n - 1)) && (col < 0 || col > (n - 1)))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
--- Battle-Field-1/GameState.cs
+++ Battle-Field-1/GameState.cs
@@ -10,21 +10,20 @@ namespace BattleField
         public static void Run()
         {
             int sizeOfBattleField = Renderer.TakeDataForGame();
-            int rows = sizeOfBattleField + 2;
-            int cols = sizeOfBattleField * 2 + 2;
+            int rows = sizeOfBattleField + GameFieldScaleOffset;
+            int cols = sizeOfBattleField * 2 + GameFieldScaleOffset;
             string[,] field = new string[rows, cols];
 
             Renderer.PrepareBattleField(sizeOfBattleField, rows, cols, field);
             Renderer.FillBattleField(sizeOfBattleField, rows, cols, field);
             Renderer.VisualizeBattleField(rows, cols, field);
 
-            int countPlayed = 0;
-            do{
-                
-                countPlayed++;
-                int x;
-                int y;
-                ReadPlayerMove(sizeOfBattleField, field, out x, out y);
+            int score = 0;
+            do
+            {
+                score++;
+                int x, y;
+                ConsoleInput.ReadPlayerMove(sizeOfBattleField, field, out x, out y);
 
                 int hitCoordinate = Convert.ToInt32(field[x, y]);
                 switch (hitCoordinate)
@@ -37,100 +36,13 @@ namespace BattleField
                 }
 
                 Renderer.VisualizeBattleField(rows, cols, field);
-               
-            }while(!IsEndOfGame(rows, cols, field));
+            }
+            while(!IsEndOfGame(rows, cols, field));
             
-            Console.WriteLine("Game over. Detonated mines: " + countPlayed);
+            Console.WriteLine("Game over. Detonated mines: " + score);
         }
 
-        private static void ReadPlayerMove(int n, string[,] workField, out int x, out int y)
-        {
-            bool isCorrectUserMove = false;
-            int row;
-            int col;
-
-            do
-            {
-                Console.WriteLine("Please enter coordinates: ");
-                String userInput = Console.ReadLine();
-                
-                bool isValidUserInput = TryParseValidUserInput(userInput, out row, out col);
-                bool areCoordinates = IsInputValidCoordinate(n, row, col);
-                bool isValidMove = false;
-
-                if (isValidUserInput && areCoordinates)
-                {
-                    isValidMove = IsPlayerValidMove(workField, row, col);
-                }
-
-                if (isValidUserInput && areCoordinates && isValidMove)
-                {
-                    isCorrectUserMove = true;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid move! ");
-                }
-            }
-            while (!isCorrectUserMove);
-
-            x = row + GameFieldScaleOffset;
-            y = col * 2 + GameFieldScaleOffset;
-        }
-
-        private static bool TryParseValidUserInput(string userInput, out int row, out int col)
-        {
-            bool isValidRow = false;
-            bool isValidCol = false;
-            string rowCol = userInput;
-            try
-            {
-                isValidRow = int.TryParse(rowCol.Substring(0, 1), out row);
-                isValidCol = int.TryParse(rowCol.Substring(2, 1), out col);
-            }
-            catch
-            {
-                // we dont care why the input is not parsed
-                // that's why no specific exception is catched
-                // we only need correct input
-                // row and col must have some int value after the method is called
-                row = -1;
-                col = -1;
-            }
-
-            if (isValidRow && isValidCol)
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-        private static bool IsPlayerValidMove(string[,] workField, int row, int col)
-        {
-            int gameFieldRow = row + GameFieldScaleOffset;
-            int gameFieldCol = col * 2 + GameFieldScaleOffset;
-            if (workField[gameFieldRow, gameFieldCol] == "-" ||
-                workField[gameFieldRow, gameFieldCol] == "X")
-            {
-                return false;
-            }
-            return true;
-        }
-
-        private static bool IsInputValidCoordinate(int n, int row, int col)
-        {
-            if ((row < 0 || row > (n - 1)) && (col < 0 || col > (n - 1)))
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-
-
-        public static bool IsEndOfGame(int rows, int cols, String[,] gameField)
+        private static bool IsEndOfGame(int rows, int cols, String[,] gameField)
         {
             bool isEndOfGame = true;
             for (int i = 2; i < rows; i++)
