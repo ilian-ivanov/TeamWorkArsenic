d3a919ba36be31b0eda84255bcdc8f32e7880d77
YanaMihaylova <yard_arm@abv.bg>
2013-05-16 22:45:22 +0300

    Added test method

---
+++ BattleField.Tests/ExplosionGeneratorTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BattleField.Tests
+{
+    [TestClass]
+    public class ExplosionGeneratorTests
+    {
+        [TestMethod]
+        public void TestDetonate_WithTinyMine()
+        {
+        }
+    }
+}
